24463a95850fe8c778f717fe11857053e3490054
egamberdiyevxushnud <xushnudegamberdiyev0@gmail.com>
2024-02-13 20:58:42 +0500

    Xushnud Egamberdiyev Qoshildi.

--- Product_API/Repositories/IProductPostgressRepository.cs
+++ Product_API/Repositories/IProductPostgressRepository.cs
@@ -1,17 +1,51 @@
-﻿using Product_API.Models;
+﻿using Npgsql;
+using Product_API.Models;
 
 namespace Product_API.Repositories
 {
     public class ProductPostgressRepository : IProductRepository
     {
+        const string connectionstring = "Server=127.0.0.1;Port=5432;Database=MongoDb;username=postgres;Password=xushnud;";
+
+
         public Product Add(Product product)
         {
-            throw new NotImplementedException();
+            using(var connection = new  NpgsqlConnection(connectionstring))
+            {
+                connection.Open();
+                string query = $"Insert into products(Name,Description,PhotoPath) values({product.Name}, {product.Description}, {product.PhotoPath})";
+                NpgsqlCommand npgsqlBatchCommand = new NpgsqlCommand(query,connection);
+                npgsqlBatchCommand.ExecuteNonQuery();
+            }
+            return product;
         }
 
         public List<Product> GetAll()
         {
-            throw new NotImplementedException();
+            using(var connection = new NpgsqlConnection(connectionstring))
+            {
+                connection.Open();
+                List<Product> products = new List<Product>();
+                Product product = new Product();
+
+                string query = $@"Select * from products";
+                NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    product.Name = reader.GetString(0);
+                    product.Description = reader.GetString(1);
+                    product.PhotoPath = reader.GetString(2);
+                    products.Add(product);
+                }
+
+                return products;
+
+
+
+            }
+            
         }
     }
 }
